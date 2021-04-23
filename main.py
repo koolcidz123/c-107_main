d862dfc39c0d247a495348ceb5051dc0b9f996a1
koolcidz123 <67968678+koolcidz123@users.noreply.github.com>
2021-04-23 19:42:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import csv
 import plotly.graph_objects as go
+import plotly.express as px
 
 df = pd.read_csv("data.csv")
 
 
 print(df.groupby('level')["attempt"].mean())
 
-fig = go.Figure(go.Bar(x =f.groupby('level')["attempt"].mean() , y=["level-1", "level-2", "level-3" , "level-4"] , orientation='h'))
+fig = px.scatter(x = df.groupby('level')["attempt"].mean() , y="level-1" , orientation='h')
 fig.show()
